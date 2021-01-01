90fd4c97d74c716d073d0e38bc939452486f1b7b
Amit Assaraf <amit@landa.app>
2021-01-01 23:31:53 +0200

    Added warning when using embedded driver to clarify instructions

--- src/views/components/ConnectionTypeSelection/index.js
+++ src/views/components/ConnectionTypeSelection/index.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Card, Dialog, Heading, Text, InfoSignIcon} from 'evergreen-ui';
+import {Button, Pane, Card, Dialog, Heading, Text, InfoSignIcon, TickCircleIcon} from 'evergreen-ui';
 import Logo from '../../../logo.png';
+import BluetoothSetting from '../../../bluetooth_setting.png';
+
 const ipcRenderer = window.require("electron").ipcRenderer;
 
 const ConnectionTypeSelection = ({}) => {
     const [showLiveChessWarning, setLiveChessWarning] = useState(false);
+    const [showEmbeddedWarning, setEmbeddedWarning] = useState(false);
 
     useEffect(() => {
         ipcRenderer.on('battery', (event, arg) => {
@@ -28,12 +31,13 @@ const ConnectionTypeSelection = ({}) => {
             <img src={Logo} width={'80%'} height={'auto'} style={{marginLeft: -16}}/>
             <Heading size={700}>Select eboard connection engine:</Heading>
             <br/>
-            <Text color="muted" size={400}>Our embedded implementation is recommended but may be incompatible with some eboard
+            <Text color="muted" size={400}>Our embedded implementation is recommended but may be incompatible with some
+                eboard
                 models.</Text>
             <br/>
             <br/>
             <Button height={40} appearance="primary" intent="success"
-                    onClick={() => ipcRenderer.send('conType', 'embedded')}>Embedded Engine (Recommended)</Button>
+                    onClick={() => setEmbeddedWarning(true)}>Embedded Engine (Recommended)</Button>
             <Button height={40} appearance="minimal" marginTop={16}
                     onClick={() => setLiveChessWarning(true)}>
                 Use traditional DGT LiveChess 2.0
@@ -48,14 +52,37 @@ const ConnectionTypeSelection = ({}) => {
                 onCloseComplete={() => setLiveChessWarning(false)}
                 confirmLabel="All Good To Go"
             >
-                <InfoSignIcon color="info" marginRight={6} />
+                <InfoSignIcon color="info" marginRight={6}/>
                 <Text size={400}>
                     Before you continue, stop now and launch DGT LiveChess 2.0 in order for the connection to work. Make
                     sure your board is connected and recognized on DGT LiveChess 2.0 before continuing.
                 </Text>
-                <br />
-                <br />
+                <br/>
+                <br/>
                 <Text color="muted" size={300}>(Note: This option is not recommended for use on MacOS)</Text>
+            </Dialog>
+
+            <Dialog
+                isShown={showEmbeddedWarning}
+                title="Embedded DGT Driver"
+                onConfirm={() => ipcRenderer.send('conType', 'embedded')}
+                onCloseComplete={() => setEmbeddedWarning(false)}
+                confirmLabel="All Good To Go"
+            >
+                <TickCircleIcon color="success" marginRight={6}/>
+                <Text size={400}>
+                    Before you continue, in order for the embedded connection to work, you must connect your
+                    DGT eboard either through USB or through your computer's Bluetooth settings.
+                </Text>
+                <Pane
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    flexDirection="column">
+                    <br/>
+                    <img src={BluetoothSetting} width={'65%'} height={'auto'}/>
+                    <br/>
+                </Pane>
             </Dialog>
         </Card>
     );
